0ee17206e78ee338da34a3dbbbfdbe201b457254
Vagelis Koutkias <koutkiasv@gmail.com>
2018-04-21 11:35:22 +0300

    Initial Commit

---
+++ ChessKnightMovement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChessKnightMovement
+//
+//  Created by Vagelis Koutkias on 4/21/18.
+//  Copyright © 2018 Vagelis Koutkias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
